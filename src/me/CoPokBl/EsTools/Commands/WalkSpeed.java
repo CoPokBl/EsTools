a82464168e808c70db22dbc0559899ad84633e21
Calcilator <skylokon@gmail.com>
2020-11-01 13:48:55 +1100

    Improved /walkspeed

--- src/me/CoPokBl/EsTools/Commands/WalkSpeed.java
+++ src/me/CoPokBl/EsTools/Commands/WalkSpeed.java
@@ -7,6 +7,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class WalkSpeed extends MultiPlayerCommand {
 
@@ -37,16 +38,37 @@ public class WalkSpeed extends MultiPlayerCommand {
         float amount;
 
         try {
-            amount = Float.parseFloat(args[0]);
+            amount = Float.parseFloat(args[0]) / 10f;
         } catch (Exception e) {
             s(sender, usage);
             return true;
         }
 
+        if (amount > 1) {
+            amount = 1;
+        } else if (amount < 0) {
+            amount = 0;
+        }
+
+
         for (Player ip : p) {
             ip.setWalkSpeed(amount);
         }
+
+        s(sender, "&aSet walk speed to &6%s", String.valueOf(amount * 10));
         return true;
     }
 
+    @Override
+    public List<String> tabComplete(CommandSender sender, String[] args, String lArg) {
+        List<String> tab = new ArrayList<String>();
+
+        if (args.length == 1) {
+            tab.add("2"); tab.add("10");
+        } else if (args.length == 2) {
+            return super.tabComplete(sender, args, lArg);
+        }
+
+        return tab;
+    }
 }
