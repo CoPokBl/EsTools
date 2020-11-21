c45960aa70af856c19fd9669ab7f54411dca819d
Calcilator <skylokon@gmail.com>
2020-11-21 11:50:20 +1100

    Added timer to god

--- src/me/CoPokBl/EsTools/Commands/God.java
+++ src/me/CoPokBl/EsTools/Commands/God.java
@@ -1,5 +1,7 @@
 package me.CoPokBl.EsTools.Commands;
 
+import me.CoPokBl.EsTools.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.LivingEntity;
@@ -8,20 +10,26 @@ import me.CoPokBl.EsTools.EntityCommand;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class God extends EntityCommand implements Listener {
-	private static ArrayList<UUID> currentPlayers = new ArrayList<>();
+	private static HashMap<UUID, BukkitRunnable> currentPlayers = new HashMap<>();
+
+	private static final String usage = genUsage("/god <entity> <time>");
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
 		LivingEntity p;
+
+		int timer = -1;
 		
 		if (args.length == 0) {
-			if (isNotEntity(sender, genUsage("/god <entity>")))
+			if (isNotEntity(sender, usage))
 				return false;
 			
 			p = (LivingEntity) sender;
@@ -30,24 +38,53 @@ public class God extends EntityCommand implements Listener {
 			
 			if (p == null)
 				return false;
+
+			if (args.length > 1) {
+				try {
+					timer = (int)(Double.parseDouble(args[1]) * 20);
+				} catch (Exception x) {
+					s(sender, usage);
+					return true;
+				}
+			}
 		}
 
 		UUID u = p.getUniqueId();
 
-		if (currentPlayers.contains(u)) {
+		BukkitRunnable r = null;
+
+		String timerStr = "forever";
+		if (timer >= 0) {
+			timerStr = String.valueOf(timer / 20d) + " seconds";
+
+			r = new BukkitRunnable() {
+				@Override
+				public void run() {
+					currentPlayers.remove(u);
+				}
+			};
+
+			r.runTaskLaterAsynchronously(Main.current, timer);
+		}
+
+		if (currentPlayers.containsKey(u)) {
+			if (currentPlayers.get(u) != null) {
+				currentPlayers.get(u).cancel();
+			}
+
 			currentPlayers.remove(u);
 			s(sender, "&aGod mode &6disabled&a for &6%s", p.getName());
 		}
 		else {
-			currentPlayers.add(u);
-			s(sender, "&aGod mode &6enabled&a for &6%s", p.getName());
+			currentPlayers.put(u, r);
+			s(sender, "&aGod mode &6enabled&a for &6%s&a for &6%s", p.getName(), timerStr);
 		}
 		return true;
 	}
 
 	@EventHandler
 	public void damage(EntityDamageEvent e) {
-		if (currentPlayers.contains(e.getEntity().getUniqueId())) {
+		if (currentPlayers.containsKey(e.getEntity().getUniqueId())) {
 			e.setCancelled(true);
 		}
 	}
