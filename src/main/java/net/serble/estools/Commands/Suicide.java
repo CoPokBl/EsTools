16fc97aec619caedcbfe123d5fec517d5106ca29
Calcilore <github@calcilore.net>
2024-03-27 00:04:21 +1100

    make /kill / added user input to suicide

--- src/main/java/net/serble/estools/Commands/Suicide.java
+++ src/main/java/net/serble/estools/Commands/Suicide.java
@@ -1,19 +1,28 @@
 package net.serble.estools.Commands;
 
-import net.serble.estools.CMD;
+import net.serble.estools.EntityCommand;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.LivingEntity;
 
-public class Suicide extends CMD {
+public class Suicide extends EntityCommand {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		
 		if (isNotPlayer(sender))
 			return false;
-		
-		LivingEntity p = (LivingEntity) sender;
+
+		LivingEntity p;
+		if (args.length == 0) {
+			p = (LivingEntity) sender;
+		}
+		else {
+			p = getEntity(sender, args[0]);
+			if (p == null) {
+				s(sender, "&cPlayer does not exist!");
+				return false;
+			}
+		}
 
 		setHealth(p, 0);
 		return true;
